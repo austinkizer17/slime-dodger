488c76e172b5f6cbe2a96cfac0af9266d87938da
Austin Kizer <austinkizer@gmail.com>
2017-06-30 18:26:18 -0500

    Updated Monster movement with velo

--- objects/monsters/monster.js
+++ objects/monsters/monster.js
@@ -1,8 +1,14 @@
 var Monster = function(x,y){
 	this.x = x;
 	this.y = y;
+	this.vx = 0;
+	this.vy = 0;
 }
-Monster.Prototype.UpdateLocation(dx,dy){
+Monster.prototype.updateLocation(dx,dy){
 	this.x += dx;
 	this.y += dy;
+}
+Monster.prototype.updateVelocity(dvx,dvy){
+	this.vx += dvx;
+	this.vy += dvy;
 }
