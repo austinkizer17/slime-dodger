143e390f26fb3806d6bca8b1cd8443389f9f2c31
Phillip Clontz <phillipac718@gmail.com>
2017-06-30 19:11:00 -0500

    Added a shell processing html file for testing

---
+++ game.js
@@ -0,0 +1,14 @@
+//Canvas Settup
+void setup() { 
+  size(400, 400);
+  background(256,0,0);
+  fill(0,0,0);
+  text("Loading...");
+} 
+var width = 400;
+var height = 400;
+
+
+void draw(){
+	rect(2, 2, 10, 10);
+}
